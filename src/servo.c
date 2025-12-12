4861e634ae2b9c02a03d1f67a1e52b747f0d8612
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2025-12-12 17:07:00 +0100

    code cleanup

--- src/servo.c
+++ src/servo.c
@@ -1,7 +1,7 @@
 #include <avr/io.h>
-#include <avr/interrupt.h> // For critical sections
+#include <avr/interrupt.h>
 #include "../inc/servo.h"
-#include "../inc/rtos.h"   // For rtos_enter/exit_critical
+#include "../inc/rtos.h"
 
 // Servo configuration for 50Hz signal (20ms period)
 // F_CPU = 16MHz, Prescaler = 8 => Tick = 0.5us
@@ -14,30 +14,18 @@
 #define ANGLE_TO_TICKS(angle) (SERVO_MIN_TICKS + ((uint32_t)(angle) * SERVO_RANGE_TICKS) / 180)
 
 void servo_init(void) {
-    // 1. Power management and pin configuration
     PRR &= ~(1 << PRTIM1);            // Enable Timer1 power
     DDRB |= (1 << PB1) | (1 << PB2);  // Set PB1 (OC1A) and PB2 (OC1B) as outputs
-
-    // 2. Reset Timer1 to safe state
     TCCR1A = 0;
     TCCR1B = 0;
     TCNT1 = 0;
     TIMSK1 = 0;     // Disable interrupts
     TIFR1 = 0xFF;   // Clear pending flags
-
-    // 3. Configure Timer1 for Fast PWM Mode 14 (TOP = ICR1)
     ICR1 = SERVO_PWM_TOP;
-    
-    // Set initial position to 90 degrees
     uint16_t initial_ticks = ANGLE_TO_TICKS(90);
     OCR1A = initial_ticks;
     OCR1B = initial_ticks;
-
-    // Configure Output Compare: Clear on Match, Set at BOTTOM (Non-inverting)
-    // Mode 14: WGM13=1, WGM12=1, WGM11=1, WGM10=0
     TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);
-    
-    // Start Timer: Prescaler = 8
     TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11);
 }
 
@@ -45,10 +33,7 @@ void servo_set(uint8_t angle) {
     if (angle > 180) {
         angle = 180;
     }
-
     uint16_t ticks = ANGLE_TO_TICKS(angle);
-
-    // Critical section for atomic 16-bit register access
     rtos_enter_critical();
     OCR1A = ticks;
     OCR1B = ticks;
